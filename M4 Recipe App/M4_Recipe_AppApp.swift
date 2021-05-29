caa5c3af9ba7bc43f61a406a3274048a5bd05dae
Bill <wpcooksey@gmail.com>
2021-05-29 16:26:19 -0600

    Initial Commit

---
+++ M4 Recipe App/M4_Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4_Recipe_AppApp.swift
+//  M4 Recipe App
+//
+//  Created by Bill on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct M4_Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeListView()
+        }
+    }
+}
